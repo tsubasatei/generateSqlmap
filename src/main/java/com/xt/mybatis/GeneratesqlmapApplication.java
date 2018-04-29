306e7eedc9b8141ae2a92b377bf5166513e26420
tsubasatei <tsubasa310@163.com>
2018-04-29 18:14:20 +0800

    mybatis 用springboot创建逆向工程

---
+++ src/main/java/com/xt/mybatis/GeneratesqlmapApplication.java
@@ -0,0 +1,12 @@
+package com.xt.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratesqlmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratesqlmapApplication.class, args);
+	}
+}
